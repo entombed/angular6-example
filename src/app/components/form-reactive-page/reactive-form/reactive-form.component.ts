c720374ee8fcfdd501ad6aabb9cc62b5e1a7a532
Andrew Bondarenko <ent101@outlook.com>
2018-09-10 17:26:51 +0300

    fix reactive form

--- src/app/components/form-reactive-page/reactive-form/reactive-form.component.ts
+++ src/app/components/form-reactive-page/reactive-form/reactive-form.component.ts
@@ -13,15 +13,17 @@ export class ReactiveFormComponent implements OnInit, OnChanges {
   userForm: FormGroup;
   checked: boolean = true;
   count: { value: number; label: string; }[];
-  chk: any;
+  // chk: any;
+  minLoginCount: number = 5;
+  pendingEmail: boolean = false;
 
   constructor() {
   }
 
-  ngOnChanges () {
-   
+  ngOnChanges() {
+
   }
-  
+
   ngOnInit() {
     this.countrys = [
       {
@@ -64,35 +66,69 @@ export class ReactiveFormComponent implements OnInit, OnChanges {
       }
     ];
 
-    this.chk = [this.count[0]];
+    // this.chk = [this.count[0]];
 
     this.userForm = new FormGroup({
-      userLogin: new FormControl('!!!!!!!!!!!!!!!!!'),
-      userPassword: new FormControl(''),
+      userAuth: new FormGroup({
+        userLogin: new FormControl(' ', [Validators.email, this.checkLength.bind(this)], this.checkEmaillIsExist.bind(this)),
+        userPassword: new FormControl('', [Validators.required, Validators.minLength(3)]),
+      }),
       userCountry: new FormControl('by'),
       userAnswer: new FormControl('n'),
       userCheckRange: new FormControl(false),
-      userRange: new FormControl({value:10, disabled: true}),
+      userRange: new FormControl({ value: 10, disabled: true }),
       userText: new FormControl(''),
       userCount: new FormControl([])
     });
-
   }
 
   submitUserForm() {
     console.dir(this.userForm)
   }
 
-  change(event) {
-    if (this.userForm.get('userCheckRange').value) {
-      this.userForm.get('userRange').enable();
-    } else {
-      this.userForm.get('userRange').disable();
-    }
-  }
-
-  changeCount(event) {
+  checkerAction(event, action) {
     console.dir(event)
+    console.dir(action)
+    switch (action) {
+      case 'checkBoxCount': {
+        
+        break;
+      };
+      case 'statusRange': {
+        // if (this.userForm.get('userCheckRange').value) {
+        //   this.userForm.get('userRange').enable();
+        // } else {
+        //   this.userForm.get('userRange').disable();
+        // }
+        this.userForm.get('userCheckRange').value ? this.userForm.get('userRange').enable() : this.userForm.get('userRange').disable();
+        break;
+      };
+    };
+  };
+
+  checkLength(control: FormControl) {
+    if (control.value.length < this.minLoginCount) {
+      return { "lengthError": true };
+    } else {
+      return null;
+    }
+  };
+
+  checkEmaillIsExist(control: FormControl): Promise<any> {
+    console.log("START")
+    this.pendingEmail = true;
+    return new Promise((resolve, reject) => {
+      setTimeout(() => {
+        if (control.value === 'q@q.q') {
+          resolve({ "emailExist": true });
+        } else {
+          resolve(null);
+        }
+        console.log("END")
+        this.pendingEmail = false;
+      }, 2000);
+    });
+
   }
 
 }
