e6d451ec28eb1ffd0ca5f01c3be398b48d99fa57
Andrew Bondarenko <ent101@outlook.com>
2018-09-14 15:34:16 +0300

    create animation page1

--- src/app/components/ex-animation-page/page1/page1.component.ts
+++ src/app/components/ex-animation-page/page1/page1.component.ts
@@ -1,15 +1,37 @@
 import { Component, OnInit } from '@angular/core';
+import { trigger, state, style, transition, animate } from '@angular/animations';
 
 @Component({
   selector: 'app-page1',
   templateUrl: './page1.component.html',
-  styleUrls: ['./page1.component.css']
+  styleUrls: ['./page1.component.css'],
+  animations: [
+    trigger('animaDiv', [
+      state('start', 
+      style({
+        backgroundColor: 'blue',
+        width: '150px',
+        height: '150px',
+      })),
+      state('end', style({
+        backgroundColor: 'red',
+        width: '300px',
+        height: '300px'
+      })),
+      transition('start => end', animate(1500)),
+      transition('end => start', animate('800ms 0.5s ease-out')),
+
+    ])
+  ]
 })
 export class Page1Component implements OnInit {
 
   constructor() { }
-
+  animaState = 'start'
   ngOnInit() {
-  }
 
+  }
+  changeAnimaState() {
+    this.animaState = this.animaState == 'start' ? 'end' : 'start';
+  }
 }
